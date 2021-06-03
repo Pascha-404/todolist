441c7458bb480599aa9ff9ad7373912fd406ec20
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-03 18:24:46 +0200

    replaced ticks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import TodoList from "./TodoList"
+import TodoList from './TodoList';
 
 function App() {
-  return (
-    <div className="App">
-      <TodoList />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<TodoList />
+		</div>
+	);
 }
 
 export default App;
