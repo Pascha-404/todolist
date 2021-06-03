6e3b50cd188b845ff37ae853ca49e7463e6c54da
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-03 18:22:04 +0200

    added method for rendering tasks

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react'
-import "./TodoList.css"
-import Todo from "./Todo"
-import NewTodoForm from "./NewTodoForm"
+import React, { Component } from 'react';
+import './TodoList.css';
+import Todo from './Todo';
+import NewTodoForm from './NewTodoForm';
 
-class TodoList extends Component{
-    constructor(props) {
-        super(props)
-        this.state = { tasks: [] }
-        this.addTask = this.addTask.bind(this)
-    }
+class TodoList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { tasks: [] };
+		this.addTask = this.addTask.bind(this);
+		this.renderTasks = this.renderTasks.bind(this);
+	}
 
-    addTask(taskObj) {
-        this.setState(curState => ({ tasks: [...curState.tasks,taskObj] }))
-        console.log(this.state.tasks)
-    }
+	addTask(taskObj) {
+		this.setState(curState => ({ tasks: [...curState.tasks, taskObj] }));
+		console.log(this.state.tasks);
+	}
 
-    render() {
-        return (
-            <section className="TodoList">
-                <NewTodoForm addTask={this.addTask}/>
-                <Todo task="Fight for peace"/>
-            </section>
-        )
-    }
+	renderTasks(evt) {
+		return this.state.tasks.map(task => (
+			<Todo task={task.task} isDone={task.isDone} id={task.id} key={task.id} />
+		));
+	}
+
+	render() {
+		return (
+			<section className='TodoList'>
+				<h1>Todo List</h1>
+				<NewTodoForm addTask={this.addTask} />
+				<div className='TodoList-tasks'>{this.renderTasks()}</div>
+			</section>
+		);
+	}
 }
 
 export default TodoList;
