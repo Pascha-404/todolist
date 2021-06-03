bde44bd4a0e0288bdde225123da91c67adf9de93
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-03 17:42:48 +0200

    first logic for adding a task

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,20 +1,41 @@
-import React, { Component } from 'react'
-import "./NewTodoForm.css"
+import React, { Component } from 'react';
+import './NewTodoForm.css';
+import { v4 as uuid } from 'uuid';
 
-class NewTodoForm extends Component{
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
+class NewTodoForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { task: '', isDone: false };
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-    render() {
-        return (
-            <form className="NewTodoForm">
-                <input type="text" id="task" name="task" placeholder="Enter your task" />
-                <button>Add Task</button>
-            </form>
-        )
-    }
+	handleSubmit(evt) {
+		evt.preventDefault();
+		const newTask = { ...this.state, id: uuid() };
+        this.props.addTask(newTask);
+        this.setState({task: ""})
+	}
+
+	handleChange(evt) {
+		this.setState({ [evt.target.name]: evt.target.value });
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit} className='NewTodoForm'>
+				<input
+					type='text'
+					id='task'
+					name='task'
+					placeholder='Enter your task'
+                    onChange={this.handleChange}
+                    value={this.state.task}
+				/>
+				<button>Add Task</button>
+			</form>
+		);
+	}
 }
 
 export default NewTodoForm;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
 import "./TodoList.css"
-import {v4 as uuid} from "uuid"
 import Todo from "./Todo"
 import NewTodoForm from "./NewTodoForm"
 
 class TodoList extends Component{
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = { tasks: [] }
+        this.addTask = this.addTask.bind(this)
+    }
+
+    addTask(taskObj) {
+        this.setState(curState => ({ tasks: [...curState.tasks,taskObj] }))
+        console.log(this.state.tasks)
     }
 
     render() {
         return (
             <section className="TodoList">
-                <NewTodoForm />
+                <NewTodoForm addTask={this.addTask}/>
                 <Todo task="Fight for peace"/>
             </section>
         )
