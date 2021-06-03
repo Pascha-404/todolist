a891e3fcd16ff29945b578e724acde0a8ba40764
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-03 18:22:20 +0200

    added classes and symbol

--- src/Todo.js
+++ src/Todo.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
-import './Todo.css'
+import React, { Component } from 'react';
+import './Todo.css';
 
 class Todo extends Component {
-    render() {
-        return (
-            <div className="Todo">
-                <p>{this.props.task}</p>
-                <button>Remove</button>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className='Todo'>
+				<p className='Todo-data'>{this.props.task}</p>
+				<button className='Todo-btn'>🗑</button>
+			</div>
+		);
+	}
 }
 
 export default Todo;
