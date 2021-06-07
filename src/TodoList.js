a5557199e52f172bb2c3260e3fdaf0aaabb497d2
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-07 20:55:10 +0200

    added comments

--- src/TodoList.js
+++ src/TodoList.js
@@ -14,6 +14,7 @@ class TodoList extends Component {
 		this.changeIsDone = this.changeIsDone.bind(this);
 	}
 
+	// sets the state with the items in localStorage (if there is no local storage -> it creats an empty one)
 	getTasks() {
 		if (!localStorage.tasks) {
 			localStorage.setItem('tasks', '[]');
@@ -24,30 +25,34 @@ class TodoList extends Component {
 		}
 	}
 
+	// gets the localStorage and parse it to an object
 	getAndParse(keyword) {
 		const getItem = localStorage.getItem(keyword);
 		return JSON.parse(getItem);
 	}
 
+	// transforms the todo-object into a string, sets the localStorage to the updated version and refreshs the state
 	stringifyAndSet(variable, keyword) {
 		const stringifyTodos = JSON.stringify(variable);
 		localStorage.setItem(keyword, stringifyTodos);
 		this.setState({ tasks: this.getTasks() });
 	}
 
+	// adds a task to the localStorage list
 	addTask(taskObj) {
-		// this.setState(curState => ({ tasks: [...curState.tasks, taskObj] }));
 		const todos = this.getAndParse('tasks');
 		todos.push(taskObj);
 		this.stringifyAndSet(todos, 'tasks');
 	}
 
+	// deletes a task from the localStorage list
 	deleteTask(taskId) {
 		const todos = this.getAndParse('tasks');
 		const filteredTodos = todos.filter(task => task.id !== taskId);
 		this.stringifyAndSet(filteredTodos, 'tasks');
 	}
 
+	// edits a task from the localStorage list
 	editTask(id, task) {
 		let tasks = this.getAndParse('tasks');
 		const idx = tasks.findIndex(task => task.id === id);
@@ -55,6 +60,7 @@ class TodoList extends Component {
 		this.stringifyAndSet(tasks, 'tasks');
 	}
 
+	// toggles the boolean of isDone in the localStorage list
 	changeIsDone(id) {
 		let tasks = this.getAndParse('tasks');
 		const idx = tasks.findIndex(task => task.id === id);
